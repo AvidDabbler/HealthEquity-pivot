5a4b9a7ecfda04d0f90b8fbee452d7eb4982f91c
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-09-08 13:36:46 -0500

    added in form able to log information

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 const custDestination = () => {
-    const form = form.querySelector('input[id="definedID"')
-    if(form === 'other'){
+    const v = document.getElementById("definedID").select.value
+    if(v === "other"){
         console.log('other')
     }
 }
 
+const gather = () => {
+    const firstNm = document.getElementById("firstNm").value;
+    const lastNm = document.getElementById("lastNm").value;
+    const pickup = document.getElementById("address").value;
+   // const dropoff = document.getElementById("dropoff").option;
+    const email = document.getElementById("email").value;
+
+    console.log(`First Name: ${firstNm}`);
+    console.log(`Last Name: ${lastNm}`);
+    console.log(`Pick-Up Location: ${pickup}`);
+    //console.log(`Drop Off Location: ${dropoff}`);
+}
+
+$(document).ready(function () {
+    /**
+     * Show regulations given an address string
+     * @param {string} input 
+     */
+
+
+      // SETUP AUTOCOMPLETE
+  // for options see here: http://api.jqueryui.com/autocomplete/
+  $("#address").autocomplete({
+    source: function (request, response) {
+      arcgisRest.suggest(request.term, {
+        params: {
+          location: '-90.18,38.62' // origin point location that is used to sort geocoding candidates based on their proximity to the location
+        }
+      }).then(function (data) {
+        response(data.suggestions.map(function (suggestionObject) {
+          return {
+            label: suggestionObject.text,
+            value: suggestionObject.text
+          };
+        }));
+      }.bind(this));
+
+    },
+    minLength: 2,
+
+  });
+    
+});
